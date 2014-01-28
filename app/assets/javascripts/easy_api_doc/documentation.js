3bda4e69a172d89ed9c3dee594438e68d6015fba
Michael Sands <michael.sands@generic.gen.nz>
2014-01-29 09:21:24 +1300

    support caching of other known auth systems

--- app/assets/javascripts/easy_api_doc/documentation.js
+++ app/assets/javascripts/easy_api_doc/documentation.js
@@ -15,18 +15,32 @@ $(document).ready(function () {
   }
   var auth_settings = {};
 
-  // load up stored authentication details if they are set
   var auth_value_cookie = $.cookie("session_settings");
   if (auth_value_cookie) {
-    var space_split = auth_value_cookie.split(" ");
-    auth_settings['type'] = space_split[0];
-    for (var i = 1; i < space_split.length; i++) {
-      var equal_split = space_split[i].split('="')
-      var trimChars = (i == (space_split.length - 1) ? -1 : -2); // always trim a '"' and trim a ',' on all but last string
-      auth_settings[equal_split[0]] = equal_split[1].slice(0, trimChars);
-      var field = $('*[name="_doc_authentication[' + equal_split[0] + ']"]');
-      if (field.val().length < 1) {
-        field.val(auth_settings[equal_split[0]]);
+    var auth_setting_pair = auth_value_cookie.split(" ");
+    auth_settings.type = auth_setting_pair[0];
+
+    if (auth_settings.type == 'basic') {
+      var auth_key_value = auth_setting_pair[1].split(':')
+      auth_settings.user = decodeBase64(auth_key_value[0]);
+      $('*[name="_doc_authentication[user]"]').val(auth_settings.user);
+      auth_settings.password = decodeBase64(auth_key_value[1]);
+      $('*[name="_doc_authentication[password]"]').val(auth_settings.password);
+    } else if (auth_settings.type == 'oauth_grant') {
+      var auth_key_value = auth_setting_pair[1].split(':')
+      auth_settings.client_id = decodeBase64(auth_key_value[0]);
+      $('*[name="_doc_authentication[client_id]"]').val(auth_settings.client_id);
+      auth_settings.secret = decodeBase64(auth_key_value[1]);
+      $('*[name="_doc_authentication[secret]"]').val(auth_settings.secret);
+    } else if (auth_settings.type == 'oauth_bearer') {
+      auth_settings.access_token = auth_setting_pair[1];
+      $('*[name="_doc_authentication[access_token]"]').val(auth_settings.access_token);
+    } else if (auth_settings.type == "OAuth") {
+      for (var i = 1; i < auth_setting_pair.length; i++) {
+        var auth_key_value = auth_setting_pair[i].split('="')
+        var trimChars = (i == (auth_setting_pair.length - 1) ? -1 : -2); // always trim a '"' and trim a ',' on all but last string
+        auth_settings[auth_key_value[0]] = auth_key_value[1].slice(0, trimChars);
+        $('*[name="_doc_authentication[' + auth_key_value[0] + ']"]').val(auth_settings[auth_key_value[0]]);
       }
     }
   }
@@ -200,9 +214,7 @@ function process_api_call(uri, method, data, auth_settings, options) {
 
       if (auth_value != null) {
         xhr.setRequestHeader("Authorization", auth_value.replace(/(\r\n|\n|\r)/gm,"")); // don't send through \n on headers or they will be ignored
-        if (!$.cookie("session_settings")) {
-          $.cookie("session_settings", auth_value);
-        }
+        $.cookie("session_settings", auth_value);
       }
 			return true;
     },
